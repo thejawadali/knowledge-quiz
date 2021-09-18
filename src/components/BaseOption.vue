cc4cba88c85370246e18e82f3a791b3aede66539
Jawad Ali <alijamil734@gmail.com>
2021-09-18 22:56:36 +0500

    made base option component

---
+++ src/components/BaseOption.vue
@@ -0,0 +1,46 @@
+<script lang="ts" setup>
+</script>
+
+
+<template>
+  <div
+    class="
+      flex
+      items-center
+      bg-white
+      cursor-pointer
+      p-5
+      w-96
+      h-16
+      overflow-hidden
+      rounded-xl
+      shadow-lg
+      hover:bg-secondary
+      group
+    "
+  >
+    <div>
+      <div
+        class="
+          bg-primary
+          w-7
+          h-7
+          text-dark
+          font-bold
+          flex
+          shadow-md
+          justify-center
+          items-center
+          rounded-full
+          group-hover:text-white group-hover:bg-dark
+        "
+      >
+        <span>A</span>
+      </div>
+    </div>
+    <p class="ml-3 text-dark text-sm group-hover:text-white">
+      Lorem ipsum dolor sit amet consectetur elit.
+    </p>
+  </div>
+</template>
+
