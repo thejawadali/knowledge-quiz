1da2f9977ae31a9f0b82d07d7105e218c311c547
Jawad Ali <alijamil734@gmail.com>
2021-09-18 15:42:19 +0500

    made 404 page

---
+++ src/views/404.vue
@@ -0,0 +1,23 @@
+<template>
+  <div
+    class="
+      bg-primary
+      rounded-lg
+      w-full
+      h-full
+      flex-col flex
+      justify-center
+      items-center
+    "
+  >
+    <div class="flex">
+      <img src="../assets/error-404.png" alt="" />
+    </div>
+    <button
+      @click="$router.push('/home')"
+      class="mt-10 bg-dark text-white px-3 py-2 rounded-md"
+    >
+      Back To Home
+    </button>
+  </div>
+</template>
